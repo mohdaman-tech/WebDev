4e05e774fb15ea15c5c1e9e58574341b69be0480
Mohd Aman Ullah Khan <amaank43@gmail.com>
2024-01-05 16:06:57 +0530

    Update and rename PythonCalculator to PythonCalculator.py

---
+++ Python Projects/PythonCalculator.py
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First </title>
+</head>
+<body style="background-color :navy;">
+        
+    <h1 style="background-color :teal">
+    </h1>
+    <br>
+    <h2 style="color :rgb(10, 238, 112)"
+    </h2>
+    <br>
+    <h3 style="color :rgb(10, 238, 112)"><i></i></h3>
+</body>
+</html>
