fe8fd645b020ba19bb09fe06f323eea8d087459f
Mohd Aman Ullah Khan <amaank43@gmail.com>
2024-01-06 16:45:21 +0530

    Update Dice.py

--- Python Projects/Dice.py
+++ Python Projects/Dice.py
@@ -6,3 +6,6 @@ num_dice = parse_input(num_dice_input)
 def parse_input(input_string):
     if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
         return int(input_string)
+    else:
+        print("Please enter a number from 1 to .")
+        raise SystemExit(1)
