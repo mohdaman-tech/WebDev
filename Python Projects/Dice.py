49024eeeda5b03a089c1e583482cc1d0740e9cb0
Mohd Aman Ullah Khan <amaank43@gmail.com>
2024-01-06 16:44:46 +0530

    Update Dice.py

--- Python Projects/Dice.py
+++ Python Projects/Dice.py
@@ -2,3 +2,7 @@
 num_dice_input = input("How many (1-6) dice do you want to roll ? ")
 
 num_dice = parse_input(num_dice_input)
+
+def parse_input(input_string):
+    if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
+        return int(input_string)
