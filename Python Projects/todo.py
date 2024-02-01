aa7545213faea2459c9f5cca5423371adaa6304c
Mohd Aman Ullah Khan <amaank43@gmail.com>
2024-02-01 22:23:52 +0530

    Update todo.py

--- Python Projects/todo.py
+++ Python Projects/todo.py
@@ -1,1 +1,6 @@
-
+print(r''' MY TODO LIST ''')
+print('''\n1.Enter ADD to add new task.
+2.Enter DEL to delete a task.
+3.Enter UPDATE to update task-list.
+4.Enter EXIT to exit from program.
+5.Enter SELECT to view your todo-list.''')
