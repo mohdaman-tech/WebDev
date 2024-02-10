cec947b5731ba80a34286576fec3dcdd16709446
Mohd Aman Ullah Khan <amaank43@gmail.com>
2024-02-10 22:08:52 +0530

    Update todo.py

--- Python Projects/todo.py
+++ Python Projects/todo.py
@@ -3,7 +3,7 @@ from prettytable import PrettyTable
 
 #Heading for Project
 print(r''' MY TODO LIST ''')
-
+print()
 #Tasks to perform
 print('''\n1.Enter ADD to add new task.
 2.Enter DEL to delete a task.
