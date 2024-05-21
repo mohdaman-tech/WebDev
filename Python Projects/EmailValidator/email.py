3861b49c77f90ca07a8afa4df90497af9d43fcb7
Mohd Aman Ullah Khan <amaank43@gmail.com>
2024-05-22 00:24:59 +0530

    Delete Python Projects directory

--- Python Projects/EmailValidator/email.py
+++
@@ -1,5 +0,0 @@
-email = int(input("Enter your email"))
-if len(email) <6:
-  print('Invalid length email')
-else:   #m@m.in
-  pass
--- Python Projects/PythonClock.py
+++
@@ -1,9 +0,0 @@
-# Import Required Library
-from tkinter import *
-import datetime
-import time
-import winsound
-from threading import *
- 
-# Create Object
-root = Tk()
